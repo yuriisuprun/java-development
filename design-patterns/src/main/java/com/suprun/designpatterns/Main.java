5d246ba6f694ecba4208d2adb0c228e295ace025
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-04-11 22:10:59 +0200

    Added null check to a method for checking is a text palindrome

---
+++ design-patterns/src/main/java/com/suprun/designpatterns/Main.java
@@ -0,0 +1,10 @@
+package com.suprun.designpatterns;
+
+/**
+ * @author Yurii_Suprun
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello design patterns!");
+    }
+}
