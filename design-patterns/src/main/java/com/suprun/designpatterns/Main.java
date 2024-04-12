a70c5dda3bec3480194d16805d2b55be9e0cc210
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-04-12 09:23:27 +0200

    Added Singleton pattern implementation

--- design-patterns/src/main/java/com/suprun/designpatterns/Main.java
+++ design-patterns/src/main/java/com/suprun/designpatterns/Main.java
@@ -1,10 +1,16 @@
 package com.suprun.designpatterns;
 
+import com.suprun.designpatterns.creational.Singleton;
+
 /**
  * @author Yurii_Suprun
  */
 public class Main {
     public static void main(String[] args) {
+
+        Singleton instance = Singleton.getInstance();
+        System.out.println(instance.toString());
+
         System.out.println("Hello design patterns!");
     }
 }
