f15f682bd95c9bf16039a255dd8837eaccb61bca
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-04-12 09:23:27 +0200

    Added Singleton pattern implementation

---
+++ design-patterns/src/main/java/com/suprun/designpatterns/creational/Singleton.java
@@ -0,0 +1,22 @@
+package com.suprun.designpatterns.creational;
+
+/**
+ * @author Yurii_Suprun
+ */
+public class Singleton {
+
+    private static Singleton instance = null;
+    private Singleton(){
+        // Optional: Prevent instantiation using reflection
+        if (instance != null) {
+            throw new IllegalStateException("Singleton instance already exists. Use getInstance() method.");
+        }
+    }
+
+    public Singleton getInstance(){
+        if (instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
