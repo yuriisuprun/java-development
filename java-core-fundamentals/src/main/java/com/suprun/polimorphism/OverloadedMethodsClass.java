f4ef3516d6523112fbe7fa70e3c96662ddb858db
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-04-19 12:02:52 +0200

    Added example of overloaded methods

---
+++ java-core-fundamentals/src/main/java/com/suprun/polimorphism/OverloadedMethodsClass.java
@@ -0,0 +1,33 @@
+package com.suprun.polimorphism;
+
+/**
+ * @author Yurii_Suprun
+ */
+public class OverloadedMethodsClass {
+
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method to add three integers
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method to add two doubles
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    // Method to concatenate two strings
+    public String add(String a, String b) {
+        return a + b;
+    }
+
+    // Method to find the maximum of two integers
+    public int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+}
+
