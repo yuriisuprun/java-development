3f01502a2d41717f300e771fd3505e8bb89253c0
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-08-11 20:16:43 +0200

    Add bubble sort algorithm

--- coding-practice/src/main/java/com/suprun/coding/Practice.java
+++ coding-practice/src/main/java/com/suprun/coding/Practice.java
@@ -202,7 +202,7 @@ public class Practice {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
-                    int temp = 1;
+                    int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
