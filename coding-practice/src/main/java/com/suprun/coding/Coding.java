024d7559d386a0fbf31a9678fa8ca13f41af3a38
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-08-14 09:38:02 +0200

    Update Coding class

--- coding-practice/src/main/java/com/suprun/coding/Coding.java
+++ coding-practice/src/main/java/com/suprun/coding/Coding.java
@@ -388,20 +388,6 @@ public class Coding {
             }
         }
         return sb.toString();
-
-//        StringBuilder result = new StringBuilder();
-//        char previousChar = str.charAt(0);
-//        result.append(previousChar);
-//
-//        for (int i = 1; i < str.length(); i++) {
-//
-//            char currentChar = str.charAt(i);
-//            if (currentChar != previousChar) {
-//                result.append(currentChar);
-//                previousChar = currentChar;
-//            }
-//        }
-//        return result.toString();
     }
 
     /**
