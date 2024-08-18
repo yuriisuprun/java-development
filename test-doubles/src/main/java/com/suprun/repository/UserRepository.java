c19ae4b4677d818492dcc495aa4cf820318fa5d3
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-08-18 17:01:41 +0200

    Update user repository

--- test-doubles/src/main/java/com/suprun/repository/UserRepository.java
+++ test-doubles/src/main/java/com/suprun/repository/UserRepository.java
@@ -3,9 +3,6 @@ package com.suprun.repository;
 import com.suprun.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 /**
  * @author Yurii_Suprun
  */
