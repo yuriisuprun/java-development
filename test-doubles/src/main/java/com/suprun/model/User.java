5c78ebbf5279cad9725f4e2397d105ffe6eceb0d
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-08-18 12:31:09 +0200

    Update User entity

--- test-doubles/src/main/java/com/suprun/model/User.java
+++ test-doubles/src/main/java/com/suprun/model/User.java
@@ -1,31 +1,23 @@
 package com.suprun.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * @author Yurii_Suprun
  */
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "app_user")
 public class User {
+
+    @Id
+    private Long id;
     private String name;
     private String email;
-
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
 
