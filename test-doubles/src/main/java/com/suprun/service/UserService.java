4fb509eb40bbe7efe23e9946dcbb2044fe9722e1
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-08-18 12:33:50 +0200

    Update user service

--- test-doubles/src/main/java/com/suprun/service/UserService.java
+++ test-doubles/src/main/java/com/suprun/service/UserService.java
@@ -1,19 +1,37 @@
 package com.suprun.service;
 
 import com.suprun.model.User;
+import com.suprun.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Yurii_Suprun
  */
+@Service
 public class UserService {
     private boolean processed = false;
+    private UserRepository userRepository;
+
+    public UserService() {}
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public void processUser(User user) {
-        // Simulate processing the user
+        // simulate processing the user
         processed = true;
     }
 
     public boolean isProcessed() {
         return processed;
+    }
+
+    public void saveUser(User user) {
+        if (userRepository != null) {
+            userRepository.save(user);
+        } else {
+            throw new IllegalStateException("UserRepository not initialized");
+        }
     }
 }
