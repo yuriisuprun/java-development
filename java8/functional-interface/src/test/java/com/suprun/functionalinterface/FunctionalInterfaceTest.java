1829e7b2934b7e5f2a6b5b0c493ebb0cf3038746
yurii-suprun <iursuprun@gmail.com>
2025-11-02 15:02:05 +0100

    Added functional interface

---
+++ java8/functional-interface/src/test/java/com/suprun/functionalinterface/FunctionalInterfaceTest.java
@@ -0,0 +1,136 @@
+package com.suprun.functionalinterface;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * A test class for {@link functionalInterface}
+ *
+ * @author Yurii_Suprun
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FunctionalInterfaceTest {
+
+    private FunctionalInterface functionalInterface;
+
+    @BeforeEach
+    void setUp() {
+        functionalInterface = new FunctionalInterface();
+    }
+
+    @Test
+    @Order(1)
+    void testGetSortedHashStringsList() {
+        String hashTagText = """
+            Going for a long run can be #podcast quite liberating, a great
+            way to #Workout clear the mind. While on my run, I usually tune into my favorite #podcast This
+            is just one of #Workout the ways I practice #SelfCare Running isn't just about fitness, it's
+            also my #MeTime So no matter how busy life gets, remember to create some space for #personalGrowth and
+            never underestimate the power of a good #Workout Your #MentalHealth will thank you!""";
+
+        List<String> expectedList = List.of("#Workout", "#podcast", "#MentalHealth", "#SelfCare", "#personalGrowth", "#MeTime");
+        List<String> actualList = functionalInterface.getSortedHashCodes(hashTagText);
+
+        assertEquals(expectedList, actualList);
+    }
+
+    @Test
+    @Order(2)
+    void testGetLengthOfLongestString() {
+        List<String> list = List.of("air", "grass", "road", "buildings");
+        int expectedLength = list.get(3).length();
+        int actualLength = functionalInterface.getLongestStringLength(list);
+
+        assertEquals(expectedLength, actualLength);
+    }
+
+    @Test
+    @Order(3)
+    void testDeleteFirstLetterFromStrings() {
+        List<String> originalList = List.of("air", "grass", "road", "buildings");
+        List<String> expectedList = List.of("uildings", "rass", "oad", "ir");
+        List<String> actualList = functionalInterface.deleteFirstLetterFromStrings(originalList);
+
+        assertEquals(expectedList, actualList);
+    }
+
+    @Test
+    @Order(4)
+    void testCountLettersForStringsLongerThan() {
+        List<String> originalList = List.of("Bill", "Tom", "Ronny", "Michelangelo", "Alexander");
+        int actualSum = functionalInterface.countLettersForStringsLongerThan(originalList, 4);
+
+        assertEquals(26, actualSum);
+    }
+
+    @Test
+    @Order(5)
+    void testFindOldestPerson() {
+        List<Person> people = List.of(new Person("Sara", 4), new Person("Viktor".intern(), 40), new Person("Eva", 42));
+        Person expectedPerson = new Person("Eva", 42);
+        Person actualPerson = functionalInterface.findOldestPerson(people);
+
+        assertEquals(expectedPerson, actualPerson);
+    }
+
+    @Test
+    @Order(6)
+    void testPartitionPeopleByAge() {
+        List<Person> people = List.of(new Person("Sara", 4), new Person("Chiara", 17),
+                new Person("Viktor".intern(), 40), new Person("Eva", 42));
+        Map<Boolean, List<Person>> expectedMap = new HashMap<>();
+        expectedMap.put(false, List.of(new Person("Sara", 4), new Person("Chiara", 17)));
+        expectedMap.put(true, List.of(new Person("Viktor".intern(), 40), new Person("Eva", 42)));
+        Map<Boolean, List<Person>> actualMap = functionalInterface.partitionPeopleByAge(people, 17);
+
+        assertEquals(expectedMap, actualMap);
+    }
+
+    @Test
+    @Order(7)
+    void testCountLettersInWord() {
+        String String = "Discussion";
+        Map<Character, Long> expectedMap = Map.of('c',1L, 's', 3L, 'D', 1L, 'u',1L, 'i', 2L, 'n', 1L, 'o', 1L);
+        Map<Character, Long> actualMap = functionalInterface.countLettersInWord(String);
+
+        assertEquals(expectedMap, actualMap);
+    }
+
+    @Test
+    @Order(7)
+    void testCountLettersInWordOldStyle() {
+        String String = "Discussion";
+        Map<Character, Long> expectedMap = Map.of('c',1L, 's', 3L, 'D', 1L, 'u',1L, 'i', 2L, 'n', 1L, 'o', 1L);
+        Map<Character, Long> actualMap = functionalInterface.countLettersInWordOldStyle(String);
+
+        assertEquals(expectedMap, actualMap);
+    }
+}
+
+
