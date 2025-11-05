01396e8e6efe350f51a15de467de33228b9bf583
yurii-suprun <iursuprun@gmail.com>
2025-11-05 15:29:32 +0100

    Added arrays tasks

---
+++ coding-practice/src/main/java/com/suprun/coding/ArrayTasks.java
@@ -0,0 +1,63 @@
+package com.suprun.coding;
+
+import java.util.Arrays;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class ArrayTasks {
+
+    public static void main(String... args) {
+        Integer[] numbers1 = {1, 2, 3, 4, 5};
+
+        int[] numbers2 = new int[6];
+        numbers2[0] = 6;
+        numbers2[1] = 7;
+        numbers2[2] = 8;
+        numbers2[3] = -3;
+        numbers2[4] = 110;
+        numbers2[5] = 11;
+
+        String[] names = {"Alice", "Bob", "Charlie"};
+
+        Integer[] numbersReversed = reverseArray(numbers1);
+        int[] numbersReversed2 = reverseIntArrayWithStream(numbers2);
+        int[] findMaximumAndMinimumElement = findMaximumAndMinimumElement(numbers2);
+        String[] namesReversed = reverseArray(names);
+        System.out.println(Arrays.toString(numbersReversed));
+        System.out.println(Arrays.toString(namesReversed));
+        System.out.println(Arrays.toString(numbersReversed2));
+        System.out.println(Arrays.toString(findMaximumAndMinimumElement));
+    }
+
+    private static <T> T[] reverseArray(T[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            T temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+        return arr;
+    }
+
+    private static int[] reverseIntArrayWithStream(int[] array) {
+        return IntStream.rangeClosed(1, array.length)
+                .map(i -> array[array.length - i])
+                .toArray();
+    }
+
+    private static int[] findMaximumAndMinimumElement(int[] array) {
+        int[] minMaxArray = new int[2];
+        int min = array[0];
+        int max = array[0];
+        for (int i : array) {
+            if (i < min) {
+                min = i;
+            }
+            if (i > max) {
+                max = i;
+            }
+        }
+        minMaxArray[0] = min;
+        minMaxArray[1] = max;
+        return minMaxArray;
+    }
+}
