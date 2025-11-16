9bf8787a6f51ee6e5c6f5fb051aa41e0139b2ab1
yurii-suprun <iursuprun@gmail.com>
2025-11-16 17:22:12 +0100

    Updated Proxy pattern example

--- design-patterns/src/main/java/com/suprun/designpatterns/structural/proxy/ProxyMain.java
+++ design-patterns/src/main/java/com/suprun/designpatterns/structural/proxy/ProxyMain.java
@@ -6,14 +6,13 @@ package com.suprun.designpatterns.structural.proxy;
 public class ProxyMain {
 
     public static void main(String... args) {
-
-        // Using the secure proxy to interact with the bank account
+        
         BankAccount bankAccount = new SecureBankAccountProxy(1000.0);
 
         bankAccount.deposit(500.0); // Valid deposit
         bankAccount.deposit(-200.0); // Invalid deposit
 
-        double balance = bankAccount.getBalance(); // Perform balance inquiry
+        double balance = bankAccount.getBalance();
 
         System.out.println("Current balance: " + balance);
     }
