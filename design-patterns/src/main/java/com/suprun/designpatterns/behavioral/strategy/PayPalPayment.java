35fb11d991543018563171b2b85605431569b0c3
yurii-suprun <iursuprun@gmail.com>
2025-11-19 00:10:43 +0100

    Added strategy pattern example

---
+++ design-patterns/src/main/java/com/suprun/designpatterns/behavioral/strategy/PayPalPayment.java
@@ -0,0 +1,15 @@
+package com.suprun.designpatterns.behavioral.strategy;
+
+public class PayPalPayment implements PaymentStrategy {
+
+    private String email;
+
+    public PayPalPayment(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println(amount + " paid using PayPal: " + email);
+    }
+}
