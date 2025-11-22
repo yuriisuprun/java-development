44663ffbf917ab25e7ead773fad2580df58347f6
yurii-suprun <iursuprun@gmail.com>
2025-11-22 12:01:17 +0100

    Added strategy pattern example

---
+++ design-patterns/src/main/java/com/suprun/designpatterns/structural/decorator/CoffeeDecorator.java
@@ -0,0 +1,21 @@
+package com.suprun.designpatterns.structural.decorator;
+
+public abstract class CoffeeDecorator implements Coffee {
+
+    protected Coffee coffee;
+
+    public CoffeeDecorator(Coffee coffee) {
+        this.coffee = coffee;
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost();
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription();
+    }
+}
+
