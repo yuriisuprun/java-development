c9b47b3e29340054e7e87f3fcd97e7e9c032a583
yurii-suprun <iursuprun@gmail.com>
2025-11-22 12:03:35 +0100

    Added strategy pattern example

---
+++ design-patterns/src/main/java/com/suprun/designpatterns/structural/decorator/MilkDecorator.java
@@ -0,0 +1,19 @@
+package com.suprun.designpatterns.structural.decorator;
+
+public class MilkDecorator extends CoffeeDecorator {
+
+    public MilkDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost() + 0.5;
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + ", milk";
+    }
+}
+
