216c62fe92100803a12503dcdacd2fc76a644732
yurii-suprun <iursuprun@gmail.com>
2025-11-30 16:16:14 +0100

    Updated FileInputStream example

--- coding-practice/src/main/java/com/suprun/coding/InputOutputStreamTasks.java
+++ coding-practice/src/main/java/com/suprun/coding/InputOutputStreamTasks.java
@@ -7,6 +7,7 @@ public class InputOutputStreamTasks {
     public static void main(String[] args) {
         writeBytesToFile();
         readBytesFromFile();
+        readWriteBytesBuffered();
     }
 
     private static void writeBytesToFile() {
@@ -23,6 +24,22 @@ public class InputOutputStreamTasks {
             int data;
             while ((data = in.read()) != -1) {
                 System.out.print((char) data);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readWriteBytesBuffered(){
+        try (
+                InputStream in = new BufferedInputStream(new FileInputStream("input_output.txt"));
+                OutputStream out = new BufferedOutputStream(new FileOutputStream("input_output.txt"))
+        ) {
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = in.read(buffer)) != -1) {
+                out.write(buffer, 0, length);
             }
         } catch (IOException e) {
             e.printStackTrace();
